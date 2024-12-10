c0f0168947621a5bae63c277ac7ca9bb18ad0b15
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2024-12-10 21:01:34 +0100

    Dynamic Programming - Dice Combinations

---
+++ DynamicProgramming/DiceCombinations.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int MOD = 1E9 + 7;
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<int> dp(n + 6);
+    dp[0] = 1;
+    for(int i = 0; i < n; i ++) {
+        for(int j = 1; j <= 6; j ++) {
+            dp[i + j] += dp[i];
+            dp[i + j] %= MOD;
+        }
+    }
+    cout << dp[n];
+    return 0;
+}
