0266679d13fb567600b19030ed395586d7a1df23
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2025-03-24 10:48:56 +0100

    Segment Tree Minimum Queries - Dynamic Range Minimum Queries

--- RangeQueries/DynamicRangeMinimumQueries.cpp
+++ RangeQueries/DynamicRangeMinimumQueries.cpp
@@ -5,29 +5,31 @@
 using namespace std;
 
 template<typename T>
-struct MinSegTree {
+struct MinSegTree 
+{
     int n = 1;
     vector<T> tree;
-    MinSegTree(const vector<T>& v) {
+    MinSegTree(const vector<T>& v) 
+    {
         while (n < v.size()) n <<= 1;
         tree.resize(n << 1, numeric_limits<T>::max());
-        for (int i = 0; i < v.size(); i ++) {
-            tree[n + i + 1] = v[i];
-        } 
-        for (int i = n - 1; i > 0; i --) {
-            tree[i] = min(tree[i << 1], tree[(i << 1) + 1]);
-        }
+        for (int i = 0; i < v.size(); i ++) tree[n + i + 1] = v[i];
+        for (int i = n - 1; i > 0; i --) tree[i] = min(tree[i << 1], tree[(i << 1) + 1]);
     }
-    T query(int l, int r) {
+    T query(int l, int r) 
+    {
         l += n;
         r += n;
         T res = numeric_limits<T>::max();
-        while (l <= r) {
-            if (l & 1) {
+        while (l <= r) 
+        {
+            if (l & 1) 
+            {
                 res = min(res, tree[l]);
                 l ++;
             }
-            if (!(r & 1)) {
+            if (!(r & 1)) 
+            {
                 res = min(res, tree[r]);
                 r --;
             }
@@ -36,36 +38,40 @@ struct MinSegTree {
         }
         return res;
     }
-    void update(int i, T x) {
+    void update(int i, T x) 
+    {
         i += n;
         tree[i] = x;
-        while ((i >> 1) > 0) {
+        while ((i >> 1) > 0) 
+        {
             i >>= 1;
             tree[i] = min(tree[i << 1], tree[(i << 1) + 1]);
         }
     }
 };
 
-int main() {
+int main() 
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int n, q;
     cin >> n >> q;
     vector<long long> v(n);
-    for (int i = 0; i < n; i ++) {
-        cin >> v[i];
-    }
+    for (int i = 0; i < n; i ++) cin >> v[i];
     MinSegTree<long long> tree(v);
-    while (q --) {
+    while (q --) 
+    {
         int t;
         cin >> t;
-        if (t == 1) {
+        if (t == 1) 
+        {
             int i;
             long long x;
             cin >> i >> x;
             tree.update(i, x);
         }
-        else {
+        else 
+        {
             int l, r;
             cin >> l >> r;
             cout << tree.query(l, r) << '\n';
