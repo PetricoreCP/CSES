57338f9d29b9856be950a51b85a664d7f71f07b5
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2025-03-24 10:45:38 +0100

    Prefix Xor Array - Range Xor Queries

--- RangeQueries/RangeXorQueries.cpp
+++ RangeQueries/RangeXorQueries.cpp
@@ -4,20 +4,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main() 
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int n, q;
     cin >> n >> q;
     vector<long long> v(n);
-    for (int i = 0; i < n; i ++) {
-        cin >> v[i];
-    }
+    for (int i = 0; i < n; i ++) cin >> v[i];
     vector<long long> prefixXor(n + 1);
-    for (int i = 0; i < n; i ++) {
-        prefixXor[i + 1] = prefixXor[i] ^ v[i];
-    }
-    while (q --) {
+    for (int i = 0; i < n; i ++) prefixXor[i + 1] = prefixXor[i] ^ v[i];
+    while (q --) 
+    {
         int l, r;
         cin >> l >> r;
         cout << (prefixXor[r] ^ prefixXor[l - 1]) << '\n';
