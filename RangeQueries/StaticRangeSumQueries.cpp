138fe11397a3d19849c2db4074d605307c9204b5
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2025-03-24 10:41:51 +0100

    Prefix Sum Array - Static Range Sum Queries

--- RangeQueries/StaticRangeSumQueries.cpp
+++ RangeQueries/StaticRangeSumQueries.cpp
@@ -4,20 +4,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main() 
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int n, q;
     cin >> n >> q;
     vector<long long> v(n);
-    for (int i = 0; i < n; i ++) {
-        cin >> v[i];
-    }
+    for (int i = 0; i < n; i ++) cin >> v[i];
     vector<long long> prefixSum(n + 1);
-    for (int i = 1; i <= n; i ++) {
-        prefixSum[i] = prefixSum[i - 1] + v[i - 1];
-    }
-    while (q --) {
+    for (int i = 1; i <= n; i ++) prefixSum[i] = prefixSum[i - 1] + v[i - 1];
+    while (q --) 
+    {
         int l, r;
         cin >> l >> r;
         cout << prefixSum[r] - prefixSum[l - 1] << '\n';
